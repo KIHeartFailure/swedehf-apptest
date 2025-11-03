d5c5511882a80e575dc9a030f363b4036780d108
Lina Benson <lina.benson@ki.se>
2025-11-03 13:10:29 +0100

    initial commit

---
+++ app.R
@@ -0,0 +1,150 @@
+source("setup.R")
+
+ui <- page_sidebar(
+  title = h4("Eligibility and event rates - SwedeHF"),
+  theme = mytheme,
+  sidebar = sidebar(
+    width = "30%",
+    fillable = FALSE,
+    card(
+      card_header(h5("Ejection fraction (%)"), class = "bg-primary"),
+      selectInput(
+        inputId = "ef",
+        label = NULL,
+        choices = appvar_values$shf_ef,
+        selected = appvar_values$shf_ef,
+        multiple = TRUE
+      )
+    ),
+    card(
+      card_header(h5("Eligibility criteria"), class = "bg-primary"),
+      selectInput(
+        inputId = "prevhfh6mo",
+        label = "HF hospitalization < 6 months",
+        choices = appvar_values$shf_sos_prevhfh6mo,
+        selected = appvar_values$shf_sos_prevhfh6mo,
+        multiple = TRUE
+      ),
+      selectInput(
+        inputId = "nyha",
+        label = "NYHA class",
+        choices = appvar_values$shf_nyha,
+        selected = appvar_values$shf_nyha,
+        multiple = TRUE
+      ),
+      selectInput(
+        inputId = "gfrckdepi",
+        label = "eGFR (mL/min/1.73 m²)",
+        choices = appvar_values$shf_gfrckdepi,
+        selected = appvar_values$shf_gfrckdepi,
+        multiple = TRUE
+      ),
+      checkboxInput("byaf", "Separate NT-proBNP by atrial fibrillation"),
+      conditionalPanel(
+        condition = "input.byaf == true",
+        selectInput(
+          inputId = "ntprobnp_af",
+          label = "NT-proBNP (pg/mL) with atrial fibrillation",
+          choices = appvar_values$shf_ntprobnp,
+          selected = appvar_values$shf_ntprobnp,
+          multiple = TRUE
+        ),
+        selectInput(
+          inputId = "ntprobnpnoaf_af",
+          label = "NT-proBNP (pg/mL) without atrial fibrillation",
+          choices = appvar_values$shf_ntprobnp,
+          selected = appvar_values$shf_ntprobnp,
+          multiple = TRUE
+        )),
+      conditionalPanel(
+        condition = "input.byaf == false",
+        selectInput(
+          inputId = "ntprobnp_noaf",
+          label = "NT-proBNP (pg/mL)",
+          choices = appvar_values$shf_ntprobnp,
+          selected = appvar_values$shf_ntprobnp,
+          multiple = TRUE
+        )),
+      selectInput(
+        inputId = "bpsys",
+        label = "Systolic blood pressure (mmHg)",
+        choices = appvar_values$shf_bpsys,
+        selected = appvar_values$shf_bpsys,
+        multiple = TRUE
+      )
+    ), card(
+      card_header(h5("Event type"), class = "bg-primary"),
+      selectInput(
+        inputId = "out",
+        label = NULL,
+        choices = outvars$name,
+        selected = outvars$name,
+        multiple = TRUE
+      )
+    )
+  ),
+  navset_card_underline(
+    nav_panel(
+      title = "Table",
+      icon = icon("table"),
+      #             page_fillable(
+      # br(),
+      uiOutput("error"),
+      # width = "250px",
+      # fill = FALSE,
+      value_box(
+        title = NULL,
+        uiOutput("eligibilty"),
+        showcase = bsicons::bs_icon("people-fill"),
+        theme = "bg-primary"
+      ),
+      # card(
+      DT::dataTableOutput(outputId = "base"),
+      padding = 0
+      # )
+      ,
+      # card(
+      DT::dataTableOutput(outputId = "event")
+      # )
+      # )
+    ),
+    nav_panel(
+      title = "Graph",
+      icon = icon("chart-simple"),
+      page_fillable(
+        uiOutput("error2"),
+        plotOutput("plot")
+      )
+    ),
+    nav_panel("Information",
+              value = "Information",
+              icon = icon("info"),
+              p("The displayed information is the last registration / patient in the Swedish Heart Failure Registry (SwedeHF) 2016-2023 with recorded Ejection fraction and discharged alive from hospital (N = 46,636)."),
+              p("The outcomes were derived from the National Patient register (hospitalizations) and The Cause of Death Register (deaths). HF was defined as ICD-10 I110, I130, I132, I255, I420, I423, I425-9, I43, I50, J81, K761, R570 and CV as I, J81, K761, R570, G45."),
+              p("The displayed information is the last registration / patient in the Swedish Heart Failure Registry (SwedeHF) 2016-2023 and discharged alive from hospital."),
+              p("Missing values are imputed with a multivariate imputation algorithm based on random forests (Mayer M (2024). _missRanger: Fast Imputation of Missing Values_. doi:10.32614/CRAN.package.missRanger")
+    )
+  )
+)
+
+
+server <- function(input, output) {
+  show_modal_spinner()
+  Sys.sleep(1.5)
+  remove_modal_spinner()
+  select_func2 <- reactive({
+    select_func(
+      ef = input$ef,
+      prevhfh6mo = input$prevhfh6mo,
+      nyha = input$nyha,
+      gfrckdepi = input$gfrckdepi,
+      ntprobnp = input$ntprobnp,
+      bpsys = input$bpsys
+    )
+  })
+  
+}
+
+
+# Create Shiny app ----
+shinyApp(ui, server)
---
+++ setup.R
@@ -0,0 +1,75 @@
+library(shiny)
+library(shinythemes)
+library(shinybusy)
+library(bslib)
+library(shinyWidgets)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(stringr)
+
+select_func <- function(ef, prevhfh6mo, nyha,
+                        af, gfrckdepi, ntprobnp, bpsys) {
+  checkempty <- c(is.null(ef), is.null(prevhfh6mo), is.null(nyha), is.null(af), is.null(gfrckdepi), is.null(ntprobnp), is.null(bpsys))
+  if (any(checkempty)) {
+    allcombsout <- 0
+  } else if (any(c(
+    diff(sort(as.numeric(ef))), diff(sort(as.numeric(prevhfh6mo))), diff(sort(as.numeric(nyha))), diff(sort(as.numeric(af))),
+    diff(sort(as.numeric(gfrckdepi))), diff(sort(as.numeric(ntprobnp))), diff(sort(as.numeric(bpsys)))
+  ) > 1)) {
+    allcombsout <- -1
+  } else {
+    allcombsout <- allcombs %>%
+      filter(shf_ef %in% paste0(sort(ef), collapse = "|") &
+        shf_sos_prevhfh6mo %in% paste0(sort(prevhfh6mo), collapse = "|") &
+        shf_nyha %in% paste0(sort(nyha), collapse = "|") &
+        shf_sos_com_af %in% paste0(sort(af), collapse = "|") &
+        shf_gfrckdepi %in% paste0(sort(gfrckdepi), collapse = "|") &
+        shf_ntprobnp %in% paste0(sort(ntprobnp), collapse = "|") &
+        shf_bpsys %in% paste0(sort(bpsys), collapse = "|")) %>%
+      pull(n)
+  }
+
+  return(allcombsout)
+}
+
+figfunc <- function(subdata) {
+  subdata <- subdata %>%
+    select(-r_0, -r_1) %>%
+    pivot_longer(cols = c(rate_0, rate_1), names_to = "el", values_to = "rate") %>%
+    filter(!is.na(rate)) %>%
+    mutate(el = factor(str_remove(el, "rate_"), levels = c("0", "1"), labels = c("Not eligible", "Eligible")))
+
+  minmax <- c(0, ceiling((max(subdata$rate) + max(subdata$rate) * .1) / 10) * 10)
+  ggplot(subdata, aes(x = time, y = rate, fill = el)) +
+    geom_bar(stat = "identity", position = "dodge") +
+    scale_y_continuous(limits = minmax, breaks = minmax) +
+    facet_wrap(vars(outname), ncol = 1) + # , scales = "free_y") +
+    scale_fill_manual(values = c("#00688B", "darkseagreen4")) +
+    theme_classic() +
+    theme(
+      text = element_text(face = "bold", size = 16),
+      legend.position = "bottom",
+      legend.box = "vertical",
+      axis.text = element_text(color = "black"),
+      axis.title.x = element_blank(),
+      legend.title = element_blank()
+    ) +
+    labs(y = "Events/100 person-years") +
+    geom_text(aes(label = round(rate, 0)), position = position_dodge(width = 0.9), vjust = -0.25, fontface = "bold")
+}
+
+mytheme <- bs_theme(
+  # Controls the default grayscale palette
+  # bg = "#DCDCDC",  fg = "#698B69",
+  # Controls the accent (e.g., hyperlink, button, etc) colors
+  primary = "#00688B", secondary = "#009ACD",
+  # base_font = c("Grandstander", "sans-serif"),
+  base_font = "'Helvetica Neue', Helvetica, sans-serif",
+  code_font = c("Courier", "monospace"),
+  heading_font = "'Helvetica Neue', Helvetica, sans-serif",
+  # Can also add lower-level customization
+  "input-border-color" = "#00688B"
+)
+
+load("./data/shinydata.RData")
